926f3a0d44d9cae13945fa2cb291fa0ae6f7f48c
Phil Gruber <philipp.gruber@anz.com>
2025-02-04 15:51:40 +1100

    bugfixes

--- internal/challenge/fixme/basic/basic_test.go
+++ internal/challenge/fixme/basic/basic_test.go
@@ -3,6 +3,7 @@ package fixme
 import (
 	"context"
 	"errors"
+	"fmt"
 	"log/slog"
 	"sync"
 	"testing"
@@ -13,9 +14,10 @@ import (
 
 // nolint
 func TestNilChannel(t *testing.T) {
-	test.ExitAfter(time.Millisecond)
+	test.ExitAfter(time.Minute)
 
-	var ch chan int
+	///	var ch chan int
+	ch := make(chan int)
 
 	go func() {
 		ch <- 1
@@ -29,7 +31,7 @@ func TestNilChannel(t *testing.T) {
 
 // nolint
 func TestClosedChannelWithoutOkCheck(t *testing.T) {
-	test.ExitAfter(time.Millisecond)
+	test.ExitAfter(time.Second)
 	ch := make(chan int)
 
 	go func() {
@@ -39,7 +41,10 @@ func TestClosedChannelWithoutOkCheck(t *testing.T) {
 
 	for {
 		select {
-		case val := <-ch:
+		case val, ok := <-ch:
+			if !ok {
+				return
+			}
 			slog.Info("received", "value", val)
 		}
 	}
@@ -50,13 +55,14 @@ func TestClosedChannelWrite(t *testing.T) {
 	defer test.ExpectNoPanic(t)
 
 	ch := make(chan int, 1)
-	close(ch)
 	ch <- 5
+	close(ch)
 }
 
 // nolint
 func TestUnlockingUnlockedLock(t *testing.T) {
 	var mu sync.Mutex
+	mu.Lock()
 	mu.Unlock()
 }
 
@@ -67,7 +73,6 @@ func TestWaitGroupNegativeCounter(t *testing.T) {
 	wg.Add(1)
 	go func() {
 		wg.Done()
-		wg.Done()
 	}()
 
 	wg.Wait()
@@ -77,7 +82,7 @@ func TestWaitGroupNegativeCounter(t *testing.T) {
 func TestContextUsingPrimitivesAsKeys(t *testing.T) {
 	type ctxKey string
 	const key ctxKey = "myKey"
-	ctx := context.WithValue(context.Background(), "myKey", "value1")
+	ctx := context.WithValue(context.Background(), key, "value1")
 
 	if val, ok := ctx.Value(key).(string); !ok || val != "value1" {
 		t.Fatalf("expected context to have 'value1' for 'myKey', got: %v", val)
@@ -89,8 +94,10 @@ func TestContextWithCancel(t *testing.T) {
 	ctx, cancelFunc := context.WithCancel(context.Background())
 
 	go func() {
-		time.Sleep(time.Second * 2)
+		time.Sleep(time.Second * 1)
+		fmt.Println("Cancelling...")
 		cancelFunc() // Cancel the context after a delay
+		fmt.Println("Done")
 	}()
 
 	select {
@@ -98,14 +105,14 @@ func TestContextWithCancel(t *testing.T) {
 		if err := ctx.Err(); !errors.Is(err, context.Canceled) {
 			t.Errorf("Expected context.Canceled, got %v", err)
 		}
-	case <-time.After(time.Second * 1):
+	case <-time.After(time.Second * 3):
 		t.Error("Context cancellation took too long")
 	}
 }
 
 // nolint
 func TestContextWithTimeout(t *testing.T) {
-	ctx, cancelFunc := context.WithTimeout(context.Background(), time.Second*2)
+	ctx, cancelFunc := context.WithTimeout(context.Background(), time.Second*1)
 	defer cancelFunc() // It's a good practice to call the cancel function even if the context times out
 
 	select {
@@ -113,14 +120,14 @@ func TestContextWithTimeout(t *testing.T) {
 		if err := ctx.Err(); !errors.Is(err, context.DeadlineExceeded) {
 			t.Errorf("Expected context.DeadlineExceeded, got %v", err)
 		}
-	case <-time.After(time.Second * 1):
+	case <-time.After(time.Second * 2):
 		t.Error("Context timeout took too long")
 	}
 }
 
 // nolint
 func TestContextWithDeadline(t *testing.T) {
-	deadline := time.Unix(22222222222, 0).Add(time.Second * 2)
+	deadline := time.Now().Add(time.Second * 2)
 	ctx, cancelFunc := context.WithDeadline(context.Background(), deadline)
 	defer cancelFunc() // It's a good practice to call the cancel function even if the context times out
 
