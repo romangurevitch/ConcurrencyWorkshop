d5263250663f07a4cfa947ed6841f421bcab4869
Anitha Ramasamy <anitha.ramasamy@anz.com>
2025-02-04 16:04:09 +1100

    test fixs

--- internal/challenge/fixme/basic/basic_test.go
+++ internal/challenge/fixme/basic/basic_test.go
@@ -15,7 +15,8 @@ import (
 func TestNilChannel(t *testing.T) {
 	test.ExitAfter(time.Millisecond)
 
-	var ch chan int
+	//var ch chan int --- Caution - should not use var
+	ch := make(chan int)
 
 	go func() {
 		ch <- 1
@@ -39,8 +40,14 @@ func TestClosedChannelWithoutOkCheck(t *testing.T) {
 
 	for {
 		select {
-		case val := <-ch:
-			slog.Info("received", "value", val)
+		case val, ok := <-ch:
+			if ok {
+				slog.Info("received", "value", val)
+			} else {
+				slog.Info("channel closed")
+				return
+			}
+			//caution -- slog.Info("received", "value", val)
 		}
 	}
 }
@@ -50,13 +57,14 @@ func TestClosedChannelWrite(t *testing.T) {
 	defer test.ExpectNoPanic(t)
 
 	ch := make(chan int, 1)
-	close(ch)
+	defer close(ch) // caution - not included defer so writing will be panic on next line
 	ch <- 5
 }
 
 // nolint
 func TestUnlockingUnlockedLock(t *testing.T) {
 	var mu sync.Mutex
+	mu.Lock()
 	mu.Unlock()
 }
 
@@ -66,8 +74,8 @@ func TestWaitGroupNegativeCounter(t *testing.T) {
 
 	wg.Add(1)
 	go func() {
-		wg.Done()
-		wg.Done()
+		defer wg.Done()
+		//wg.Done() --- caution --- two done statements for wg
 	}()
 
 	wg.Wait()
@@ -98,7 +106,7 @@ func TestContextWithCancel(t *testing.T) {
 		if err := ctx.Err(); !errors.Is(err, context.Canceled) {
 			t.Errorf("Expected context.Canceled, got %v", err)
 		}
-	case <-time.After(time.Second * 1):
+	case <-time.After(time.Second * 3):
 		t.Error("Context cancellation took too long")
 	}
 }
@@ -113,14 +121,14 @@ func TestContextWithTimeout(t *testing.T) {
 		if err := ctx.Err(); !errors.Is(err, context.DeadlineExceeded) {
 			t.Errorf("Expected context.DeadlineExceeded, got %v", err)
 		}
-	case <-time.After(time.Second * 1):
+	case <-time.After(time.Second * 3):
 		t.Error("Context timeout took too long")
 	}
 }
 
 // nolint
 func TestContextWithDeadline(t *testing.T) {
-	deadline := time.Unix(22222222222, 0).Add(time.Second * 2)
+	deadline := time.Now().Add(time.Second * 2)
 	ctx, cancelFunc := context.WithDeadline(context.Background(), deadline)
 	defer cancelFunc() // It's a good practice to call the cancel function even if the context times out
 
