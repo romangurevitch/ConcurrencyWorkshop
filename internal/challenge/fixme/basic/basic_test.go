8f854de2640def6dbae1d1b34f99827716b3a98d
jarodlam <10664255+jarodlam@users.noreply.github.com>
2025-02-04 15:46:23 +1100

    Basic solutions

--- internal/challenge/fixme/basic/basic_test.go
+++ internal/challenge/fixme/basic/basic_test.go
@@ -16,11 +16,10 @@ func TestNilChannel(t *testing.T) {
 	cancelFn := test.ExitWithCancelAfter(context.Background(), time.Second)
 	defer cancelFn()
 
-	var ch chan int
+	ch := make(chan int)
 
 	go func() {
 		ch <- 1
-		close(ch)
 	}()
 
 	for val := range ch {
@@ -42,7 +41,10 @@ func TestClosedChannelWithoutOkCheck(t *testing.T) {
 
 	for {
 		select {
-		case val := <-ch:
+		case val, ok := <-ch:
+			if !ok {
+				return
+			}
 			slog.Info("received", "value", val)
 		}
 	}
@@ -53,13 +55,14 @@ func TestClosedChannelWrite(t *testing.T) {
 	defer test.ExpectNoPanic(t)
 
 	ch := make(chan int, 1)
-	close(ch)
 	ch <- 5
+	close(ch)
 }
 
 // nolint
 func TestUnlockingUnlockedLock(t *testing.T) {
 	var mu sync.Mutex
+	mu.Lock()
 	mu.Unlock()
 }
 
@@ -70,7 +73,6 @@ func TestWaitGroupNegativeCounter(t *testing.T) {
 	wg.Add(1)
 	go func() {
 		wg.Done()
-		wg.Done()
 	}()
 
 	wg.Wait()
@@ -80,7 +82,7 @@ func TestWaitGroupNegativeCounter(t *testing.T) {
 func TestContextUsingPrimitivesAsKeys(t *testing.T) {
 	type ctxKey string
 	const key ctxKey = "myKey"
-	ctx := context.WithValue(context.Background(), "myKey", "value1")
+	ctx := context.WithValue(context.Background(), key, "value1")
 
 	if val, ok := ctx.Value(key).(string); !ok || val != "value1" {
 		t.Fatalf("expected context to have 'value1' for 'myKey', got: %v", val)
@@ -92,7 +94,7 @@ func TestContextWithCancel(t *testing.T) {
 	ctx, cancelFunc := context.WithCancel(context.Background())
 
 	go func() {
-		time.Sleep(time.Second * 2)
+		time.Sleep(time.Millisecond * 500)
 		cancelFunc() // Cancel the context after a delay
 	}()
 
@@ -108,7 +110,7 @@ func TestContextWithCancel(t *testing.T) {
 
 // nolint
 func TestContextWithTimeout(t *testing.T) {
-	ctx, cancelFunc := context.WithTimeout(context.Background(), time.Second*2)
+	ctx, cancelFunc := context.WithTimeout(context.Background(), time.Millisecond*500)
 	defer cancelFunc() // It's a good practice to call the cancel function even if the context times out
 
 	select {
@@ -123,7 +125,7 @@ func TestContextWithTimeout(t *testing.T) {
 
 // nolint
 func TestContextWithDeadline(t *testing.T) {
-	deadline := time.Unix(22222222222, 0).Add(time.Second * 2)
+	deadline := time.Now().Add(time.Second * 2)
 	ctx, cancelFunc := context.WithDeadline(context.Background(), deadline)
 	defer cancelFunc() // It's a good practice to call the cancel function even if the context times out
 
