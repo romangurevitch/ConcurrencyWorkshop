77b3e04081c334149cee4da5dc88d1b4c09030b1
Phil Gruber <philipp.gruber@anz.com>
2025-02-06 15:25:35 +1100

    basic challenge

--- internal/challenge/implme/basic/arithmetics/arithmetics.go
+++ internal/challenge/implme/basic/arithmetics/arithmetics.go
@@ -1,23 +1,37 @@
 package arithmetics
 
 import (
+	"sync"
+	"sync/atomic"
 	"time"
 )
 
 func SequentialSum(inputSize int) int {
 	sum := 0
-	for i := 1; i <= inputSize; i++ {
-		sum += process(i)
+	for i := 1; i <= int(inputSize); i++ {
+		sum += int(process(int64(i)))
 	}
 	return sum
 }
 
 // ParallelSum implement this method.
-func ParallelSum(_ int) int {
-	panic("implement me!")
+func ParallelSum(inputSize int) int {
+	var sum atomic.Int64
+	wg := sync.WaitGroup{}
+	wg.Add(inputSize)
+	for i := 1; i <= inputSize; i++ {
+		i := i
+		go func() {
+			defer wg.Done()
+			sum.Add(process(int64(i)))
+		}()
+	}
+	wg.Wait()
+	return int(sum.Load())
+
 }
 
-func process(num int) int {
+func process(num int64) int64 {
 	time.Sleep(time.Millisecond) // simulate processing time
 	return num * num
 }
